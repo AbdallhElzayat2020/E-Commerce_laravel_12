8651fb834abe18332fe1ebf5d51cafff076cbeeb
Abdallh ELzayat <abdallhelzayat.dev@gmail.com>
2025-09-26 09:25:48 +0300

    Enhance Arabic authentication language support and add login view templates for improved user experience

--- lang/ar/auth.php
+++ lang/ar/auth.php
@@ -1,27 +1,46 @@
 <?php
 
 return [
+
     /*
     |--------------------------------------------------------------------------
     | Authentication Language Lines
     |--------------------------------------------------------------------------
     |
     | The following language lines are used during authentication for various
-    | messages that we need to display to the user. You are free to modify
+    | includes that we need to display to the user. You are free to modify
     | these language lines according to your application's requirements.
     |
     */
 
-    'failed'   => 'بيانات الاعتماد هذه غير متطابقة مع البيانات المسجلة لدينا.',
-    'throttle' => 'عدد كبير جدا من محاولات الدخول. يرجى المحاولة مرة أخرى بعد :seconds ثانية.',
+    'failed' => 'بيانات الاعتماد هذه غير مطابقة لسجلاتنا.',
+    'password' => 'كلمة المرور  غير صحيحة.',
+    'throttle' => 'عدد محاولات تسجيل الدخول كبير جدًا. يرجى المحاولة مرة أخرى خلال :seconds ثانية.',
+
+
     'remember_me' => 'تذكرني',
-    'forget_password' => 'هل نسيت كلمة المرور؟',
     'login' => 'تسجيل الدخول',
-    'register' => 'تسجيل',
-    'email' => 'البريد الالكتروني',
-    'password' => 'كلمة المرور',
-    'confirm_password' => 'تأكيد كلمة المرور',
+    'forgot_password' => 'نسيت كلمة المرور؟',
+    'register' => ' حساب جديد',
     'logout' => 'تسجيل الخروج',
-    'name' => 'الاسم',
-    'not_match' => 'بيانات الاعتماد غير متطابقه'
+    'email' => 'البريد الإلكتروني',
+    'password_filed' => 'كلمة المرور',
+    'not_match' => 'بيانات الاعتماد غير متطابقة',
+    'success_login' => 'تم تسجيل الدخول بنجاح.',
+    'success_logout' => 'تم تسجيل الخروج بنجاح.',
+    'success_register' => 'تم التسجيل بنجاح. يرجى التحقق من بريدك الإلكتروني لتأكيد حسابك.',
+    'forget_password' => 'نسيت كلمة المرور؟',
+    'send_reset_link' => 'إرسال رابط إعادة تعيين كلمة المرور',
+    'forget_password_text' => 'سوف نرسل لك رابطًا لإعادة تعيين كلمة المرور.',
+    'show_otp_form' => 'التحقق بخطوتين',
+    'otp_send' => 'التحقق',
+    'otp' => 'الرجاء إدخال رمز التحقق المرسل إلى بريدك الإلكتروني',
+    'otp_msg' => 'تم إرسال رمز التحقق إلى بريدك الإلكتروني.',
+    'reset_password' => 'إعادة تعيين كلمة المرور',
+    'password_confirmation' => 'تأكيد كلمة المرور',
+    'otp_verification' => 'التحقق بخطوتين',
+    'otp_code' => 'رمز التحقق',
+    'save' => 'حفظ',
+
+    'reset_password_success' => 'تم إعادة تعيين كلمة المرور بنجاح. يرجى تسجيل الدخول باستخدام كلمة المرور الجديدة.',
 ];
