b2ef352580308a69de49ce3c79b40e5b86266d31
Abdallh ELzayat <abdallhelzayat.dev@gmail.com>
2025-12-18 15:37:49 +0200

    solve rotes error

--- resources/views/frontend/pages/about-us.blade.php
+++ resources/views/frontend/pages/about-us.blade.php
@@ -6,7 +6,7 @@
 <section class="blog about-blog">
     <div class="container">
         <div class="blog-bradcrum">
-            <span><a href="index-2.html">Home</a></span>
+            <span><a href="{{ route('website.home') }}">Home</a></span>
             <span class="devider">/</span>
             <span><a href="#">About Us</a></span>
         </div>
